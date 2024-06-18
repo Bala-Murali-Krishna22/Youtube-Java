dcc90bd9b3d31615a9af85932cb9853895394c10
balu22 <balubmk22@gmail.com>
2024-06-18 10:42:25 +0530

    Abstraction practiced

---
+++ oops_Abstraction3/AtmSbi.java
@@ -0,0 +1,21 @@
+package oops_Abstraction3;
+
+public class AtmSbi extends AtmAbstract{
+
+	public void withdrawl() {
+		System.out.println("SBI withdrawl code");
+	}
+
+	public void transfer() {
+		System.out.println("SBI transfer code");
+	}
+
+	public void balanceEnquiry() {
+		System.out.println("SBI balance-enquiry code");
+	}
+
+	public void pinChange() {
+		System.out.println("SBI pin-change code");
+	}
+
+}
