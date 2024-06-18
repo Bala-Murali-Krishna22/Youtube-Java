2a96144a70954174e9956e83336a0458f19091a1
balu22 <balubmk22@gmail.com>
2024-06-18 11:09:04 +0530

    In polymorphism method overloading practiced

---
+++ oops_Polymorphism_MethodOverloading4/Addition.java
@@ -0,0 +1,17 @@
+package oops_Polymorphism_MethodOverloading4;
+
+public class Addition {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+
+	public double add(int a, double b) {
+		return a+b;
+	}
+	
+	public double add(double a, double b) {
+		return a+b;
+	}
+
+}
